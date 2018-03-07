9664791204157d4f75a18fb896abdfc201cb4650
kristian <37063983+kristian-94@users.noreply.github.com>
2018-03-08 00:31:42 +1100

    Added some error checking for emails and names should format correctly

--- user_upload.php
+++ user_upload.php
@@ -19,7 +19,6 @@ $query = "SELECT * FROM users";
 $result = mysqli_query($conn, $query);
 
 
-// table did not exist. Create it.
     if (!$result){
 
         // Create the table users in this database 'userinfo' if doesn't exist.
@@ -32,7 +31,8 @@ $result = mysqli_query($conn, $query);
             die("creating table did not work. " . mysqli_connect_error());   
         }
         else {
-            echo "table users created.";} 
+            echo "table users created.";
+        } 
     }
 
 // Table did exist
@@ -46,33 +46,108 @@ $result = mysqli_query($conn, $query);
 
 // open file in read only mode
 $file = fopen("users.csv","r");
-// while not at end of file keep going thru each line
-    while(!feof($file))
-      {
-        $ArrayOneLine = fgetcsv($file);
+
+
+
+$allEmails=array();
+
+// should read table first then insert all current emails into $allEmails before trying to add new ones.
+
+$query = "SELECT email FROM users";
+$result = mysqli_query($conn, $query);
+    
+    if (!$result){
+        die ('Query FAILED, could not read.');
         
-        $lowerName = strtolower($ArrayOneLine[0]);
-        $finalName= mysqli_real_escape_string($conn, ucwords($lowerName));
-        
-        $lowerSurname = strtolower($ArrayOneLine[1]);
-        $finalSurname= mysqli_real_escape_string($conn, ucwords($lowerSurname));
-        
-        $lowerEmail = strtolower($ArrayOneLine[2]);
-        $finalEmail = mysqli_real_escape_string($conn, $lowerEmail);
-        
-        
-        $query = "INSERT INTO users(name, surname, email)";
-        $query .= "VALUES ('$finalName', '$finalSurname', '$finalEmail')";
-        
-        $result = mysqli_query($conn, $query);
-        
-        if (!$result){
-            die ("Query FAILED" . mysqli_error($conn));
-        
+    } 
+while ($row = mysqli_fetch_assoc($result)) {
+            array_push($allEmails, $row["email"]);
         }
-            else {echo "users table updated. <br>";} 
+
+// Handy function from http://www.media-division.com/correct-name-capitalization-in-php/
+// Get correct capitalisation on various names.
+function titleCase($string) 
+{
+	$word_splitters = array(' ', '-', "O'", "L'", "D'", 'St.', 'Mc');
+	$lowercase_exceptions = array('the', 'van', 'den', 'von', 'und', 'der', 'de', 'da', 'of', 'and', "l'", "d'");
+	$uppercase_exceptions = array('III', 'IV', 'VI', 'VII', 'VIII', 'IX');
+ 
+	$string = strtolower($string);
+	foreach ($word_splitters as $delimiter)
+	{ 
+		$words = explode($delimiter, $string); 
+		$newwords = array(); 
+		foreach ($words as $word)
+		{ 
+			if (in_array(strtoupper($word), $uppercase_exceptions))
+				$word = strtoupper($word);
+			else
+			if (!in_array($word, $lowercase_exceptions))
+				$word = ucfirst($word); 
+ 
+			$newwords[] = $word;
+		}
+ 
+		if (in_array(strtolower($delimiter), $lowercase_exceptions))
+			$delimiter = strtolower($delimiter);
+ 
+		$string = join($delimiter, $newwords); 
+	} 
+	return $string; 
+}
+
+
+
+
+// while not at end of file keep going thru each line
+    while(!feof($file)){
         
+        //cycle through and place parts of csv in different variables.
+        $row = fgetcsv($file);
+        
+        $fixedFirstName = titleCase($row[0]);
+        $finalName= mysqli_real_escape_string($conn, $fixedFirstName);
+        
+        $fixedSurname = titleCase($row[1]);
+        $finalSurname= mysqli_real_escape_string($conn, $fixedSurname);
+        
+        $lowerEmail = filter_var(strtolower($row[2]), FILTER_SANITIZE_EMAIL);
+        //$finalEmail = mysqli_real_escape_string($conn, $lowerEmail);
+        $finalEmail = $lowerEmail;
+
+        
+        
+        //check if email is unique or has been inserted before.
+                
+                if (in_array($finalEmail, $allEmails)){
+
+                    //echo "ERROR adding " . $finalEmail . ": email address must be unique.<br>";
+                }   else if (!filter_var($finalEmail, FILTER_VALIDATE_EMAIL)) {
+                                
+                    echo "ERROR adding email, invalid email format: " . $finalEmail . "<br>"; 
+                    }
+        
+                else {
+                    $query = "INSERT INTO users(name, surname, email)";
+                    $query .= "VALUES ('$finalName', '$finalSurname', '$finalEmail')";
+
+                    $result = mysqli_query($conn, $query);
+
+                        if (!$result){
+                            //die ("Query FAILED, because: " . mysqli_error($conn));
+
+                        }
+                        else {
+                                echo "users table updated. <br>";
+                                array_push($allEmails, $finalEmail);
+                            }
+                }
+        
+
     }
+
+
+
 $query = "SELECT * FROM users";
 $result = mysqli_query($conn, $query);
     
@@ -83,7 +158,7 @@ $result = mysqli_query($conn, $query);
 
   while ($row = mysqli_fetch_assoc($result)) {
             print_r($row);
-            echo "we are here.";
+            echo "<br>";
         }
 
 
