9080fe7d21a4480caa1d72ab7b9b1dc1b3b53b88
kristian <37063983+kristian-94@users.noreply.github.com>
2018-03-11 13:10:36 +1100

    simple solution for counter

---
+++ counter.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+    
+    // Set min and max values for the numbers to be outputted. 
+    $minumum = 1;
+    $maximum = 100;
+    
+    /*
+    // Loop through numbers and print 
+    for ($counter = $minumum; $counter<$maximum+1; $counter++) {
+        
+        if ($counter%3 == 0 && $counter%5 == 0) {
+            echo "triplefiver <br>";
+        }
+        else if ($counter%3 == 0){
+            
+            echo "triple<br>";
+        }
+        else if ($counter%5 == 0){
+            
+            echo "fiver<br>";
+        }
+        else {
+            echo $counter . "<br>";
+        }
+        
+    }*/
+    
+    $time_start = microtime(true); 
+    
+    
+   /* for ($i = 0; $i<1000; $i++) {
+        
+        for ($counter = $minumum; $counter<$maximum+1; $counter++) {
+
+            $outString = "";
+
+            if ($counter%3 == 0) {
+                $outString .= "triple";
+            }
+            if ($counter%5 == 0){ 
+                $outString .= "fiver";
+            }
+            if ($outString == ""){
+                $outString = $counter;
+            }
+
+            $outString .= "<br>";
+
+            echo $outString;
+
+        }
+		}
+		*/
+	
+		    // FIRST METHOD
+        for ($counter = $minumum; $counter<$maximum+1; $counter++) {
+
+            if ($counter%3 == 0 && $counter%5 == 0) {        
+                    echo "triplefiver";       
+            }
+            else if ($counter%3 == 0){
+                echo "triple";
+            }
+            else if ($counter%5 == 0){
+                echo "fiver";
+            }
+            else {
+                echo $counter;
+            }
+            
+            echo "<br>";
+
+        }
+    
+    echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
+    
+?>
+</body>
+</html>
