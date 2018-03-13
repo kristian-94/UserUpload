037e61b26c6bd83ccad4c74bbd5d7e69cfed8161
kristian <37063983+kristian-94@users.noreply.github.com>
2018-03-13 16:24:28 +1100

    fixed up comments

--- counter.php
+++ counter.php
@@ -4,59 +4,10 @@
     $minumum = 1;
     $maximum = 100;
     
-    /*
-    // Loop through numbers and print 
-    for ($counter = $minumum; $counter<$maximum+1; $counter++) {
-        
-        if ($counter%3 == 0 && $counter%5 == 0) {
-            echo "triplefiver <br>";
-        }
-        else if ($counter%3 == 0){
-            
-            echo "triple<br>";
-        }
-        else if ($counter%5 == 0){
-            
-            echo "fiver<br>";
-        }
-        else {
-            echo $counter . "<br>";
-        }
-        
-    }*/
-    
- //$time_start = microtime(true); 
-    
-    
-   /* for ($i = 0; $i<1000; $i++) {
-        
-        for ($counter = $minumum; $counter<$maximum+1; $counter++) {
-
-            $outString = "";
-
-            if ($counter%3 == 0) {
-                $outString .= "triple";
-            }
-            if ($counter%5 == 0){ 
-                $outString .= "fiver";
-            }
-            if ($outString == ""){
-                $outString = $counter;
-            }
-
-            $outString .= "<br>";
-
-            echo $outString;
-
-        }
-		}
-		*/
-	
-		    // FIRST METHOD
         for ($counter = $minumum; $counter<$maximum+1; $counter++) {
 
             if ($counter%3 == 0 && $counter%5 == 0) {        
-                    echo "triplefiver";       
+            	echo "triplefiver";       
             }
             else if ($counter%3 == 0){
                 echo "triple";
@@ -69,9 +20,5 @@
             }
             
             echo "\n";
-
         }
-    
-  //  echo 'Total execution time in seconds: ' . (microtime(true) - $time_start);
-    
 ?>
